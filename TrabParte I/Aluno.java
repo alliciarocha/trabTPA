dfb40d29ed9d283aea558e99422868a0689790e0
Allicia Rocha <155778524+alliciarocha@users.noreply.github.com>
2025-04-08 10:25:08 -0300

    Adicionado Classe Aluno
    - Getters e Setters
    - toString()

---
+++ TrabParte I/Aluno.java
@@ -0,0 +1,39 @@
+public class Aluno {
+    private int matricula;
+    private String nome;
+    private float nota;
+
+    public Aluno(int matricula, String nome, float nota){
+        this.matricula = matricula;
+        this.nome = nome;
+        this.nota = nota;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno: " + this.nome + "," + "Matrícula: " + this.matricula + "," + "Nota" + this.nome;
+    }
+}
